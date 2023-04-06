6f61579d5b29d7f945160718c3432d9b448a6bae
Leonardo Tsutomu Yamashiro <leotyama@hotmail.com>
2023-04-06 10:31:18 -0300

    Add files via upload

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,13 @@
+import { createBox } from "framer-motion"
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-createBox;
+  }
+  *, input, button {
+    font-family: 'Roboto', sans-serif
+  }
+`
